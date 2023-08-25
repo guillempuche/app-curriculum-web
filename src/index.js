f8a454f573d2e7cf63b17126f8ea7ac456042753
Guillem Puche <3519924+guillempuche@users.noreply.github.com>
2023-08-25 20:34:44 +0200

    Add routing, redirect home page to privacy policy page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+
+import { PrivacyPolicyScreen } from './privacy_policy_screen';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/privacy" element={<PrivacyPolicyScreen />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 );
+
+function Home() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		navigate('/privacy');
+		console.log('home');
+	}, [navigate]);
+
+	return null;
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
